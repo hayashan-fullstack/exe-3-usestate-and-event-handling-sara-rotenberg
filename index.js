40b411e00f0025d5dfa1d99aa446bcee17ed5007
sara-rotenberg <57223181+sara-rotenberg@users.noreply.github.com>
2019-12-16 17:56:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,76 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Head = () => {
+    return (
+        <div>
+            <table border = '1'>
+            <tr>
+                <th width="24%">details</th>
+                <th width="25%">deadline</th>
+                <th width="25%">important</th>
+                <th width="24%">done</th>
+            </tr>
+            </table>
+        </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+    )
+}
+
+const Print = (props) => {
+    return (
+        <div>
+            <table border = '1'>
+            <tr>
+                <td width="25%">{props.line.details}</td>
+                <td width="26%">{props.line.deadline}</td>
+                <td width="27%">{props.line.important}</td>
+                <td width="25%">{props.line.done}</td>
+            </tr>
+            </table>
+        </div>
+
+    )
+}
+
+const Printall = (props) => {
+    return(
+        props.toDoList.map(line=><Print line={line}/>)
+    )
+};
+
+export const App = () => {
+    const toDoList = [
+        {
+            details: "sara",
+            deadline: "01/01/2000",
+            important: "important",
+            done: "V"
+        },
+        {
+            details: "rivka",
+            deadline: "12/04/2019",
+            important: " ",
+            done: "V"
+        },
+        {
+            details: "rachel",
+            deadline: "01/09/2012",
+            important: "important",
+            done: " "
+        },
+    ];
+    return (
+        <div style={{margin:'15%'}}>
+        <br/>
+        <br/>
+            <h1>My Todo</h1>
+            <br/>
+            <Head/>
+            <Printall toDoList={toDoList}/> 
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
